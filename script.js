a08c8c953c0aacc2d81bd42dba92a72d530f2516
Efosa Oviawe <josephoviawe4@gmail.com>
2021-10-28 12:59:02 +0100

    dashboard page updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // menu toggle
-
+/*
 (function(){
     "use strict";
 
@@ -19,4 +19,39 @@
         
     });
 
+}());*/
+
+(function(){
+    'use strict'
+    const submenu = document.querySelectorAll("ul li ul");
+
+    function hideSubMenu(){
+        //console.log(submenu)
+        for(let i=0; i<submenu.length; i++){
+            submenu[i].className ="hide-menu";
+        }
+    }
+    hideSubMenu()
+
+
+    const menuLink = document.querySelectorAll(".menu-link");
+    //console.log(menuLink)
+
+    for(let i=0; i<menuLink.length;i++){
+        menuLink[i].addEventListener('click', function(evt){
+            evt.preventDefault();
+            //alert("clicked!")
+            const thisMenu = this.parentNode.querySelector('ul');
+            //alert(thisMenu.innerHTML);
+
+            if (thisMenu.classList.contains('hide-menu')){
+
+                hideSubMenu()
+                thisMenu.className ="show-menu";
+            }else{
+                thisMenu.className ="hide-menu";
+            }
+        })
+    } 
+    
 }());
