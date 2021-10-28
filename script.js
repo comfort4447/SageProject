12e071a4704fe1a9e5dcdc1d28cb8d3cec106f52
Tayo Comfort <tayoabisola44@gmail.com>
2021-10-28 13:10:19 +0100

    index

---
+++ script.js
@@ -0,0 +1,10 @@
+// Flexslider
+$(window).load(function(){
+    $(".flexslider").flexslider({
+        animation: "slide",
+        slideshowSpeed: 4000,
+        direction: "horizontal",
+        reverse: true,
+        pauseOnHover: true,
+    });
+});
